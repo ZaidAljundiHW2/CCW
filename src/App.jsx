1c8eb2819a502b1ea3926e725d7750d231ff08ff
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2026-06-30 01:43:52 +0300

    Set up react environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+      <div className='bg-red-500 w-[50px] h-[50px]'></div>
+      
+    </div>
+  )
+}
+
+export default App
