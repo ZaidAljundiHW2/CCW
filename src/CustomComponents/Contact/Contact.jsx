ac7221fa3276dbc7411af8246c74b50f328b27c1
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2026-07-06 02:31:59 +0300

    Completed Contact Styling

--- src/CustomComponents/Contact/Contact.jsx
+++ src/CustomComponents/Contact/Contact.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Back2 from '@/assets/img/back2.png'
-import { Flex, Box } from '@chakra-ui/react'
-import { Input } from '@chakra-ui/react'
+import { Flex, Box, Text } from '@chakra-ui/react'
 import ContactForm from './ContactForm'
+import Wave2 from '@/assets/icons/waveicon2.png'
+import './Contact.css'
+import InstagramIcon from '@/assets/icons/instagram.png'
+import FacebookIcon from '@/assets/icons/facebook.png'
+import TiktokIcon from '@/assets/icons/tiktok.png'
+import BoilBag from '@/assets/img/boil-bag-cutout-transparent.png'
+import Tray from '@/assets/img/catering-trays-source.png'
+import Back3 from '@/assets/img/back3.png'
 
 const Contact = () => {
   return (
@@ -21,22 +28,20 @@ const Contact = () => {
     
     >
 
-        <h1>
+        <h1 className='CUH'>
             CONTACT US
         </h1>
 
-        <Flex className='flex-1 bg-yellow-500 gap-5' padding={'2%'}>
+        <Flex className='flex-1 bg-radial from-[#012446] from-40% to-[#1d4163] rounded-lg shadow-lg gap-5' padding={'2%'}>
 
             {/* Contact Form */}
-
             <Flex
                 className='
                     flex-col
                     flex-1
-                    bg-green-500
                     rounded-lg
                     shadow-lg
-
+                    bg-[#F2F0EF]
                 '   
 
                 style={{
@@ -62,9 +67,86 @@ const Contact = () => {
             <Flex
                 className='
                     flex-1
-                    bg-blue-500
+                    
+                    rounded-lg
+                    shadow-lg
+                    flex-col
+                    relative
                 '
+
+                
+
+                style={{
+                    padding:'20px',
+                    backgroundImage:`url(${Back3})`
+                }}
             >
+
+                <img 
+                    src={BoilBag}
+                    alt='boil bag'
+
+                    className='CUI'
+                />
+
+                <img 
+                    src={Tray}
+                    alt='boil bag'
+
+                    className='CUI2'
+                />
+
+                <Flex className='flex-col gap-0' paddingBottom={'20px'}>
+
+                    <h1 className='CUH2'>
+                        FOLLOW US
+                    </h1>
+
+                    <img src={Wave2} className='w-[150px]'/>
+
+
+                </Flex>
+
+                <Flex className='flex-1 flex-col gap-5' paddingLeft={'20px'}>
+
+                    <Flex gap={'3'} className='items-center'>
+
+                        <img src={InstagramIcon} style={{
+                            height:'40px',
+                            width:'40px'
+                        }}/>
+                        <Text className='CUT'>
+                            @captainscrab.ca
+                        </Text>
+
+                    </Flex>
+
+                    <Flex gap={'3'} className='items-center'>
+
+                        <img src={TiktokIcon} style={{
+                            height:'40px',
+                            width:'40px'
+                        }}/>
+                        <Text className='CUT'>
+                            @captainscrab.ca
+                        </Text>
+
+                    </Flex>
+
+                    <Flex gap={'3'} className='items-center'>
+
+                        <img src={FacebookIcon} style={{
+                            height:'40px',
+                            width:'40px'
+                        }}/>
+                        <Text className='CUT'>
+                            /captainscrab.ca
+                        </Text>
+
+                    </Flex>
+
+                </Flex>
+
 
             </Flex>
 
--- src/CustomComponents/Contact/ContactForm.jsx
+++ src/CustomComponents/Contact/ContactForm.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
-import { Input, Flex, Button } from '@chakra-ui/react'
+import { Input, Flex, Button, Textarea, Field  } from '@chakra-ui/react'
 import { FaRegPaperPlane } from "react-icons/fa6";
+import './ContactForm.css'
 
 const ContactForm = () => {
   return (
     <div className='flex-col flex gap-5'>
 
+        <h1 className='CFH'>
+            Send Us A Message!
+        </h1>
+
         <Flex
             className='gap-5'
         >
-
-            <Input placeholder='Name'/>
-            <Input placeholder='Email'/>
+            <Input placeholder='Name*' required/>
+            
+            <Input placeholder='Email*' required/>
 
         </Flex>
+        
+
 
         <Input placeholder='Phone Number'/>
 
-        <Input placeholder='Subject'/>
+        <Input placeholder='Subject*' required/>
 
-        <Input placeholder='Message...' style={{
+        <Textarea placeholder='Message...*' required style={{
             height:'103px'
-        }}/>
+        }} />
 
-        <Button className='w-[100px]'>
+        <Button className='w-[100px]' bg={'orange'} color={'white'}>
             Submit
             <FaRegPaperPlane />
         </Button>
