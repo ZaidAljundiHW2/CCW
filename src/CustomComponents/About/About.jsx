28454052b27d71fdae837ce93846da9beb4c479a
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2026-07-12 03:39:27 +0300

    created loader component

--- src/CustomComponents/About/About.jsx
+++ src/CustomComponents/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Flex } from '@chakra-ui/react'
 import AboutUsBack from '@/assets/img/aboutusback.png'
+import './About.css'
 
 const About = () => {
   return (
@@ -26,8 +27,22 @@ const About = () => {
                 flex-1
                 rounded-lg
                 shadow-xl
+                flex-col
             '
+
+            style={{
+                padding:'40px'
+            }}
         >
+
+            <h1 className='auhead'>
+                About Us
+            </h1>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
 
         </Flex>
 
