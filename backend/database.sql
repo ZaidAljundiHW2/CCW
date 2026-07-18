7f064adc52c54a142cbd12ce72770c9ab1a3735e
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2026-07-19 01:48:39 +0300

    setting up backend

---
+++ backend/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE CaptainsCrabDatabase;
+
+CREATE TABLE Menu (
+
+    FoodItemID SERIAL PRIMARY KEY,
+    ItemName varchar(255),
+    HasDesc boolean,
+    ItemDescription text,
+    FoodImage VARCHAR(500),
+
+);
