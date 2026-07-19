7fbcdb65a29b4559537e0bd86dcd8e93bdc36491
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2026-07-19 22:07:22 +0300

    Update database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -9,3 +9,44 @@ CREATE TABLE Menu (
     FoodImage VARCHAR(500)
 
 )
+
+INSERT INTO menu (
+    itemname, 
+    hasdesc, 
+    itemdescription, 
+    foodimage,
+    itemtype,
+    price
+) VALUES (
+    'Snow Crab Leg Boil',
+    TRUE,
+    'Sweet snow crab legs, shrimp, corn, and potatoes seasoned to perfection.',
+    'https://drive.google.com/file/d/1cUH5cogkz57SvVsLEdclgMZE8W_-cCDr/view?usp=sharing',
+    'Main Course',
+    '54.99'
+
+)
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Lemonade', FALSE, '', 'https://drive.google.com/file/d/1HQhyRLHd4_bpRndZl3cfKfdkKiLhCCpG/view?usp=sharing', 'Drinks', '3.49');
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Strawberry Mojito', FALSE, '', 'https://drive.google.com/file/d/1o_kJ3R1yONLOeQb7eXvJXvlcEBloDYU-/view?usp=sharing', 'Drinks', '5.99');
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Water', FALSE, '', 'https://drive.google.com/file/d/1COlouG6W2KAbEeJ1eZ6FWjO9I7VNJBI8/view?usp=sharing', 'Drinks', '1.99');
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Coca Cola', FALSE, '', 'https://drive.google.com/file/d/1wMU-8EaozpQ4YWDI77B6eje2bVkxI2pb/view?usp=sharing', 'Drinks', '2.99');
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Pepsi', FALSE, '', 'https://drive.google.com/file/d/1PH8Xj9QlOL8v5ud4ykcWLg_k5IBO9WfP/view?usp=sharing', 'Drinks', '2.99');
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Fanta', FALSE, '', 'https://drive.google.com/file/d/10ep_vhNTPuo5kDmxtTi6szOdqEtZPjkK/view?usp=sharing', 'Drinks', '2.99');
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Sprite', FALSE, '', 'https://drive.google.com/file/d/1ORQHEZNLT4Vh7EQaRqO7wK9gn1iHaV0R/view?usp=sharing', 'Drinks', '2.99');
+
+INSERT INTO menu (itemname, hasdesc, itemdescription, foodimage, itemtype, price)
+VALUES ('Orange Juice', FALSE, '', 'https://drive.google.com/file/d/1paZ-IehYLdtKEseGvfez-8Baw-HYPFVD/view?usp=sharing', 'Drinks', '2.99');
