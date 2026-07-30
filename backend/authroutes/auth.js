ba8ff1d128800b465e6ceff4670439022ff0345f
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2026-07-30 21:37:17 +0300

    cookie debugging

--- backend/authroutes/auth.js
+++ backend/authroutes/auth.js
@@ -10,7 +10,7 @@ const cookieOptions = {
 
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: 'Strict',
+    sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000
 }
 
