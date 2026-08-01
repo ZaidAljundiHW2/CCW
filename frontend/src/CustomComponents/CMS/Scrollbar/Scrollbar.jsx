f8683d2134af8212d56520c693aa10955ec99227
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2026-08-01 04:35:14 +0300

    cms burger

--- frontend/src/CustomComponents/CMS/Scrollbar/Scrollbar.jsx
+++ frontend/src/CustomComponents/CMS/Scrollbar/Scrollbar.jsx
@@ -1,144 +1,356 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Scrollbar.css'
 import { Link } from 'react-router-dom'
-import { Button, Menu, Portal } from "@chakra-ui/react"
-import { CiMenuBurger } from "react-icons/ci";
-import { color } from 'motion';
-import { useState } from 'react';
+import { Menu, Portal, Box } from "@chakra-ui/react"
+
 
 const Scrollbar = () => {
 
-    const [openmenu, setopenmenu] = useState(false);
+
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 
-  return (
-    <div
-        className='
-            w-[20vw]
-            portrait:w-full
-            bg-[#012447]
-            portrait:bg-white
-            landscape:h-screen
-            landscape:overflow-y-scroll
-            flex-col
-            flex
-            scrollbar
-            lanscape:items-center
-            h-full
-            z-50
-        '
 
-        
-    >   
+    return (
 
-        <div className='portrait:hidden'>
+        <div
 
-            <h1 className='text-center'>
+            className='
+                w-[20vw]
+                portrait:w-full
+                bg-[#012447]
+                landscape:h-screen
+                landscape:overflow-y-scroll
+                flex-col
+                flex
+                scrollbar
+                landscape:items-center
+                h-full
+                z-50
+            '
 
-                Captain's Crab Admin CMS
+        >
 
-            </h1>
 
-            <Link className='w-full' to={'Menu'}>
-                <button>
-                    Menu
-                </button>
-            </Link>
 
-            <Link className='w-full' to={'Location'}>
-                <button>
-                    Locations
-                </button>
-            </Link>
+            {/* DESKTOP SIDEBAR */}
 
-            <Link className='w-full' to={'General'}>
-                <button>
-                    General Details
-                </button>
-            </Link>
+            <div className='portrait:hidden'>
 
-            <Link className='w-full' to={'About'}>
-                <button>
-                    About
-                </button>
-            </Link>
 
-            <Link className='w-full' to={'Contact'}>
-                <button>
-                    Contact Queries
-                </button>
-            </Link>
+                <h1 className='text-center text-white'>
 
-            <Link className='w-full' to={'Franchise'}>
-                <button>
-                    Franchise Queries
-                </button>
-            </Link>
+                    Captain's Crab Admin CMS
 
-            <Link className='w-full' to={'Book'}>
-                <button>
-                    Booking Reservation Queries
-                </button>
-            </Link>
+                </h1>
+
+
+
+                <Link className='w-full' to={'Menu'}>
+
+                    <button>
+
+                        Menu
+
+                    </button>
+
+                </Link>
+
+
+
+                <Link className='w-full' to={'Location'}>
+
+                    <button>
+
+                        Locations
+
+                    </button>
+
+                </Link>
+
+
+
+                <Link className='w-full' to={'General'}>
+
+                    <button>
+
+                        General Details
+
+                    </button>
+
+                </Link>
+
+
+
+                <Link className='w-full' to={'About'}>
+
+                    <button>
+
+                        About
+
+                    </button>
+
+                </Link>
+
+
+
+                <Link className='w-full' to={'Contact'}>
+
+                    <button>
+
+                        Contact Queries
+
+                    </button>
+
+                </Link>
+
+
+
+                <Link className='w-full' to={'Franchise'}>
+
+                    <button>
+
+                        Franchise Queries
+
+                    </button>
+
+                </Link>
+
+
+
+                <Link className='w-full' to={'Book'}>
+
+                    <button>
+
+                        Booking Reservation Queries
+
+                    </button>
+
+                </Link>
+
+
+            </div>
+
+
+
+
+
+
+
+            {/* MOBILE DROPDOWN MENU */}
+
+
+            <div
+
+                className='
+                    landscape:hidden
+                    fixed
+                    bg-[#012447]
+                    flex
+                    w-[20%]
+                    justify-start
+                    border-white
+                '
+
+                style={{
+
+                    padding:'5px',
+                    borderWidth:'2px'
+
+                }}
+
+            >
+
+
+
+                <Menu.Root
+
+                    open={mobileMenuOpen}
+
+                    onOpenChange={(details) => 
+                        setMobileMenuOpen(details.open)
+                    }
+
+                >
+
+
+
+                    <Menu.Trigger asChild>
+
+
+                        <Box
+
+                            className='dropdown-trigger burger'
+
+                            aria-label='Open menu'
+
+                        >
+
+
+                            <span 
+                                className={
+                                    mobileMenuOpen 
+                                    ? 'burger-open' 
+                                    : ''
+                                }
+                            ></span>
+
+
+                            <span 
+                                className={
+                                    mobileMenuOpen 
+                                    ? 'burger-open' 
+                                    : ''
+                                }
+                            ></span>
+
+
+                            <span 
+                                className={
+                                    mobileMenuOpen 
+                                    ? 'burger-open' 
+                                    : ''
+                                }
+                            ></span>
+
+
+                        </Box>
+
+
+                    </Menu.Trigger>
+
+
+
+
+
+                    <Portal>
+
+
+                        <Menu.Positioner>
+
+
+                            <Menu.Content>
+
+
+
+                                <Link to={'Menu'}>
+
+                                    <Menu.Item>
+
+                                        Menu
+
+                                    </Menu.Item>
+
+                                </Link>
+
+
+
+
+                                <Link to={'Location'}>
+
+                                    <Menu.Item>
+
+                                        Locations
+
+                                    </Menu.Item>
+
+                                </Link>
+
+
+
+
+
+                                <Link to={'General'}>
+
+                                    <Menu.Item>
+
+                                        General Details
+
+                                    </Menu.Item>
+
+                                </Link>
+
+
+
+
+
+                                <Link to={'About'}>
+
+                                    <Menu.Item>
+
+                                        About
+
+                                    </Menu.Item>
+
+                                </Link>
+
+
+
+
+
+                                <Link to={'Contact'}>
+
+                                    <Menu.Item>
+
+                                        Contact Queries
+
+                                    </Menu.Item>
+
+                                </Link>
+
+
+
+
+
+                                <Link to={'Franchise'}>
+
+                                    <Menu.Item>
+
+                                        Franchise Queries
+
+                                    </Menu.Item>
+
+                                </Link>
+
+
+
+
+
+                                <Link to={'Book'}>
+
+                                    <Menu.Item>
+
+                                        Booking Reservation Queries
+
+                                    </Menu.Item>
+
+                                </Link>
+
+
+
+                            </Menu.Content>
+
+
+                        </Menu.Positioner>
+
+
+                    </Portal>
+
+
+                </Menu.Root>
+
+
+
+            </div>
+
+
+
 
         </div>
 
-        <div className='landscape:hidden fixed bg-white flex w-[20%] justify-start border-black' style={{padding:'5px', borderWidth:'2px'}}>
-            <Menu.Root>
-                <Menu.Trigger asChild>
-                    <Button
-                    variant="ghost"
-                    bg="transparent"
-                    _active={{ bg: 'transparent' }}
-                    _hover={{ bg: 'transparent' }}
-                    p={0}
-                    className="menu-trigger-btn"
-                    >
-                    <span style={{ display: 'inline-flex', fontSize: '2.5rem' }}>
-                        <CiMenuBurger style={{ width: '1em', height: '1em' }} />
-                    </span>
-                    </Button>
-                </Menu.Trigger>
-                <Portal>
-                    <Menu.Positioner>
-                    <Menu.Content>
-                        <Link to={'Menu'}>
-                            <Menu.Item>Menu</Menu.Item>
-                        </Link>
+    )
 
-                        <Link to={'Location'}>
-                            <Menu.Item>Locations</Menu.Item>
-                        </Link>
-
-                        <Link to={'General'}>
-                            <Menu.Item>General Details</Menu.Item>
-                        </Link>
-
-                        <Link to={'About'}>
-                            <Menu.Item>About</Menu.Item>
-                        </Link>
-
-                        <Link to={'Contact'}>
-                            <Menu.Item>Contact Queries</Menu.Item>
-                        </Link>
-
-                        <Link to={'Franchise'}>
-                            <Menu.Item>Franchise Queries</Menu.Item>
-                        </Link>
-
-                        <Link to={'Book'}>
-                            <Menu.Item>Booking Reservation Queries</Menu.Item>
-                        </Link>
-                    </Menu.Content>
-                    </Menu.Positioner>
-                </Portal>
-            </Menu.Root>
-        </div>
-
-
-      
-    </div>
-  )
 }
+
 
 export default Scrollbar
